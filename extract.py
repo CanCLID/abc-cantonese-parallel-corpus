be2f568cdbbf8543026a7383bb661d9a5a63ac79
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-11-02 01:38:06 +0800

    Text correction

--- extract.py
+++ extract.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import regex as re
 from glob import glob
+import unicodedata
 
 substitution_yue = (
     ('!', '！'),
@@ -12,10 +13,12 @@ substitution_yue = (
     ('嚫', '親'),
     ('衭', '褲'),
     ('贃', '賺'),
+    ('𡄯', '噎'),
     ('𠶧', '掂'),
     ('𠹺', '埋'),
     ('𡁵', '緊'),
     ('𡃶', '錫'),
+    ('𧨾', '氹'),
     ('依𠺢', '依家'),
     ('而𠺢', '而家'),
     ('𠺢吓', '家下'),
@@ -24,9 +27,20 @@ substitution_yue = (
     ('自覺得己', '覺得自己'),
 )
 
+trans = None
+
+def full_width_to_half_width(s: str) -> str:
+    global trans
+    if trans is None:
+        fw = 'ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ１２３４５６７８９０'
+        hw = unicodedata.normalize('NFKC', fw)
+        trans = str.maketrans(fw, hw)
+    return s.translate(trans)
+
 def normalise(yue: str, en: str) -> tuple[str, str]:
     for src, dst in substitution_yue:
         yue = yue.replace(src, dst)
+    yue = full_width_to_half_width(yue)
     return yue, en
 
 filename = glob('Wenlin+Dictionaries-*.xml')[-1]
@@ -42,7 +56,8 @@ for page in soup.select('page'):
 
     for match in pattern.finditer(content):
         yue, en = match.groups((1, 2))
-        if yue == '(empty band???)' or en == '(empty band???)':
+        if '(empty band???)' in (yue, en) or \
+                '[missing example characters???]' in (yue, en):
             continue
         assert '\n' not in yue
         assert '\n' not in en
